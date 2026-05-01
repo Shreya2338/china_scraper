99121e36bd24c93f66c7c4153dde04f35969abd6
Shreya Sharma <shreya2338@gmail.com>
2026-05-01 13:02:58 -0400

    Fix 2

--- china_scraper.py
+++ china_scraper.py
@@ -1,12 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "http://www.icama.org.cn/zwgk/nyzj/nyzjcpdj/"
 
 headers = {
-    "User-Agent": "Mozilla/5.0"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+    "Referer": "http://www.icama.org.cn/",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive"
 }
 
-response = requests.get(url, headers=headers)
+session = requests.Session()
+response = session.get(url, headers=headers)
+
 response.encoding = "utf-8"
 
-print(response.text[:2000])
+print(response.text[:1000])  # debug againpython china_scraper.py
