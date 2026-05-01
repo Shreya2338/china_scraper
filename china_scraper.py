ea63a92af95a3b91644869029f380de290b947a0
Shreya Sharma <shreya2338@gmail.com>
2026-05-01 13:01:44 -0400

    Fixx 1

--- china_scraper.py
+++ china_scraper.py
@@ -1,5 +1,4 @@
 import requests
-from bs4 import BeautifulSoup
 
 url = "http://www.icama.org.cn/zwgk/nyzj/nyzjcpdj/"
 
@@ -10,23 +9,4 @@ headers = {
 response = requests.get(url, headers=headers)
 response.encoding = "utf-8"
 
-soup = BeautifulSoup(response.text, "html.parser")
-
-links = soup.find_all("a")
-
-data = []
-
-for link in links:
-    title = link.text.strip()
-    href = link.get("href")
-
-    if title and href and "html" in href:
-        data.append({
-            "title": title,
-            "link": "http://www.icama.org.cn" + href
-        })
-
-print("TOTAL ITEMS:", len(data))
-
-for item in data[:10]:
-    print(item)
+print(response.text[:2000])
